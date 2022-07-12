4f58a0afea60853c2f8426b6978cffe148ee5b15
Jayden Callan <jqcallan04@gmail.com>
2022-07-12 11:09:54 -0600

    initial commit

---
+++ MethodOverloading/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MethodOverloading
+{
+    class Program
+    {
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        public static decimal Add(decimal num1, decimal num2)
+        {
+            return num1 + num2;
+        }
+        public static string Add(int num1, int num2, bool bool1)
+        {
+            if (bool1 == true && num1+num2 == 1)
+            {
+                return $"{num1 + num2} dollar";
+            }
+            else if (bool1 == true)
+            {
+                return $"{num1 + num2} dollars";
+            }
+            else
+            {
+                return $"{num1 + num2}";
+            }
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
